4d058c850d9dfbfc5399a24591e85c418f5563a1
bernardusrendy <bernardusrendy38@gmail.com>
2019-11-18 18:45:44 +0700

    NEED TO ADD SHOW WIN OR LOSE AT WHAT ROUND and ROUND TRANSITION in bootstrap popup modal + Sound effects

--- SerialPort_Nodejs_Arduino/public/scripts.js
+++ SerialPort_Nodejs_Arduino/public/scripts.js
@@ -1,14 +1,4 @@
-// Game Variables
-var id;
-var supply=0;
-var demand=0;
-var score=0;
-var grid_phase=0;
-var duration=10000;
-var game=0;
-var grid_phase_duration = 500;
-
-//Audio Files
+// *************************************************************************AUDIO FILES****************************************************** //
 var transientAu =new Audio(); transientAu.src = "/Audio/TransientFade.wav";
 var steadyAu =new Audio(); steadyAu.src = "/Audio/steady.wav";
 var lockAu =new Audio(); lockAu.src = "/Audio/Lock.wav";
@@ -20,9 +10,11 @@ var tickAu =new Audio(); tickAu.src = "/Audio/Tick.wav";
 var tickUpAu =new Audio(); tickUpAu.src = "/Audio/TickUp.wav";
 var humAu =new Audio(); humAu.src = "Hum.wav"; humAu.loop = true;
 
+// ***********************************************************************OBJECT ORIENTED DECLARATION**************************************************** //
+
 // Class Declaration
 class Generator {
-  constructor(power,fail_chance,number,state,button,limit_switch,transient_time,time_turned_on) {
+  constructor(power,fail_chance,number,transient_time,state,button,limit_switch) {
     this.power = power;
     this.fail_chance = fail_chance;
     this.number = number;
@@ -30,26 +22,18 @@ class Generator {
     this.button = button;
     this.limit_switch = limit_switch;
     this.transient_time = transient_time;
-    this.time_turned_on = time_turned_on;
-  }
-}
-
-class Main_Control{
-  constructor(supply,demand,score,grid_phase){
-    this.supply = supply;
-    this.demand = demand;
-    this.score = score;
-    this.grid_phase = grid_phase;
   }
 }
 
 // Object Call
-gen1 = new Generator(1,0.1,1,"offline",0,0,1000);
-gen2 = new Generator(3,0.2,2,"offline",0,0,1000);
-gen3 = new Generator(4,0.2,3,"offline",0,0,2000);
-gen4 = new Generator(5,0.2,4,"offline",0,0,2000);
-gen5 = new Generator(7,0.3,5,"offline",0,0,2000);
-gen6 = new Generator(12,0.4,6,"offline",0,0,2000);
+gen1 = new Generator(1,0.1,1,1000);
+gen2 = new Generator(3,0.2,2,1000);
+gen3 = new Generator(4,0.2,3,2000);
+gen4 = new Generator(5,0.2,4,2000);
+gen5 = new Generator(7,0.3,5,2000);
+gen6 = new Generator(12,0.4,6,2000);
+
+// ***********************************************************************MQTT AND SERIAL COMMUNICATION PERIPHERALS**************************************************** //
 
 // Server and broker address
 const brokerAddress = 'localhost'
@@ -86,7 +70,6 @@ client.on('connect', function() {
 
 // Message Receive
 client.on('message', function(topic, message) {
-    //console.log('received message on %s: %s', topic, message)
     switch (topic) {
         case "1/button":
             gen1.button=Number(message);
@@ -125,7 +108,6 @@ client.on('message', function(topic, message) {
             gen6.limit_switch=Number(message);
             break;
     }
-    // console.log(supply);
 })
 
 // Message send
@@ -133,9 +115,13 @@ function serialOut(message){
   client.publish(message);
 }
 
+
+// ***********************************************************************CHANGES EXECUTOR TO HTML AND VARIABLE VALUE**************************************************** //
+
 function changePower(generator,power){
   id="power_"+generator.number.toString();
-  document.getElementById(id).innerHTML = power.toString();
+  generator.power=power;
+  document.getElementById(id).innerHTML = generator.power.toString();
 }
 
 function changeState(generator,state){
@@ -155,7 +141,6 @@ function changeState(generator,state){
   else if(generator.state=="fail"){
     $(id).removeClass("bg-danger");
   }
-  //=================
   if(state=="offline"){
     $(id).addClass("bg-secondary");
     transientAu.pause(); transientAu.currentTime = 0.0;
@@ -184,7 +169,6 @@ function changeState(generator,state){
   }
   generator.state=state;
   serialOut(generator.number.toString+"/state/"+generator.state);
-  // console.log("perubahan warna");
 }
 
 function changeDemand(number){
@@ -286,6 +270,86 @@ function generatorState(generator){
   }
 }
 
+function gameStart(){
+  clearInterval(intervalGrid);
+  game=1;
+  console.log("Game Started");
+  gridPhase();
+}
+
+function gameOver(){
+  game=-1;
+  console.log("Game Stopped");
+}
+
+function randomPower(generator,floor,range){
+  changePower(generator,floor+Math.floor(Math.random()*range));
+}
+
+function randomDemand(minimumOn){
+  powerArray=[gen1.power,gen2.power,gen3.power,gen4.power,gen5.power,gen6.power];
+  totalDemand=0;
+  do{  
+    for (i=0;i<6;i++){
+      randomizerArray[i]=Math.round(Math.random());
+    }
+    for (i=0;i<6;i++){
+      if(randomizerArray[i]==1){
+        on++;
+      }
+    }
+  } 
+  while (on<minimumOn);
+  for (i=0;i<6;i++){
+    if(randomizerArray[i]==1){
+      totalDemand=totalDemand+powerArray[i];
+    }
+  }
+  changeDemand(totalDemand);
+}
+
+function round(minimumOn,duration){
+  clearInterval(intervalCount);
+  randomPower(gen1,3,4);
+  randomPower(gen2,5,4);
+  randomPower(gen3,5,4);
+  randomPower(gen4,7,2);
+  randomPower(gen5,9,2);
+  randomPower(gen6,11,2);
+  randomDemand(minimumOn);
+  countDown(duration,gameOver,"time");
+}
+
+// ***********************************************************************HTML EVENT HANDLER**************************************************** //
+
+document.getElementById("mulai").onclick = function gameOverClicked(){
+  gameStart();
+}
+
+document.getElementById("stop").onclick = function gameOverClicked(){
+  gameOver();
+}
+
+// ***********************************************************************INTERVAL THREADS**************************************************** //
+
+// CheckRound interval check
+function checkRound(){
+  var startTime = Date.now();
+  intervalCheckRound = setInterval(function() {
+      var elapsedTime = Date.now() - startTime;
+      var distance = 30 - elapsedTime;
+      if (distance <= 6) {
+        startTime=Date.now();
+        if(demand==supply){
+          clearInterval(intervalCheckRound);
+          roundNumber++;
+          game=2;
+        }
+      }
+  },10);
+}
+
+// gridPhase interval change
 function gridPhase(){
   var startTime = Date.now();
   intervalGrid = setInterval(function() {
@@ -298,35 +362,21 @@ function gridPhase(){
   },10);
 }
 
-
-document.getElementById("mulai").onclick = function gameStart(){
-  game=1;
-  console.log("game Started");
-}
-document.getElementById("stop").onclick = function gameOverClicked(){
-  gameOver();
-}
-
-//Countdown and game
-function gameOver(){
-  game=-1;
-  console.log("game Stopped");
-}
-
-// Timer countDown
-function countDown(Duration, func, id){
+// Timer countDown interval
+function countDown(duration, func, id){
   var startTime = Date.now();
   intervalCount = setInterval(function() {
       var elapsedTime = Date.now() - startTime;
-      var distance = Duration - elapsedTime;
+      var distance = duration - elapsedTime;
       document.getElementById(id).innerHTML = (distance / 1000).toFixed(2);
-      if (distance <= 6) {
+      if (distance <= 10) {
         clearInterval(intervalCount);
         return func();
       }
-  },10);
+  },5);
 }
 
+// CheckGeneratorState interval to change generator state
 function checkGeneratorState(){
     var startTime = Date.now();
     var interval = setInterval(function() {
@@ -346,46 +396,90 @@ function checkGeneratorState(){
     },10);
 }
 
+// ***********************************************************************MICROCONTROLLER-LIKE PROCEDURAL**************************************************** //
+
+// Game Variables
+var id;
+var supply=0;
+var demand=0;
+var score=0;
+var grid_phase=1;
+var game=0;
+var grid_phase_duration = 500;
+var roundNumber=0; // roundNumber==0 means the game is on idle event handling
+var win=0;
+var randomizerArray=[0,0,0,0,0,0];
+var on=0;
+var i=0;
+var totalDemand=0;
+var powerArray=[gen1.power,gen2.power,gen3.power,gen4.power,gen5.power,gen6.power];
+
+// Interval Purpose Global Variables
+var intervalCount;
+var intervalGrid;
+var intervalCheckRound;
+
 // Microcontroller like setup and loop
 setup();
 var startTime = Date.now();
 var interval = setInterval(function() {
       var elapsedTime = Date.now() - startTime;
-      var distance = 50- elapsedTime;
+      var distance = 30- elapsedTime;
       if (distance <= 6) {
-        startTime=Date.now();
+        startTime=Date.now(); 
         return loop();
       }
   },10)
-var intervalDemand;
-var intervalCount;
-var intervalGrid;
 
 function setup(){
-
+  checkGeneratorState();
 }
 
 function loop(){
+  // game==0 means the game is on idle event handling, no loop will be executed (reduces program's load)
+  // game==1 means event handler senses Game Started
   if(game==1){
-    game=0;
-    var duration=9000;
-    countDown(duration,gameOver,"time");
-    gridPhase();
-    checkGeneratorState();
-    var startTime1 = Date.now();
-    intervalDemand = setInterval(function() {
-          var elapsedTime = Date.now() - startTime1;
-          var distance = 5000 - elapsedTime;
-          if (distance <= 6) {
-            startTime1=Date.now();
-            return changeDemand(Math.random()*25);
-          }
-      },10)
+    game=2;
+    roundNumber=1;
   }
+  // game==2 means event handler senses Round Changes
+  else if(game==2){
+    game=0;
+    document.getElementById("roundNumber").innerHTML = roundNumber.toString();
+    if (roundNumber==1){
+      round(1,10000);
+    }
+    else if (roundNumber==2){
+      round(2,9000);
+    }
+    else if (roundNumber==3){
+      round(3,9000);
+    }
+    else if (roundNumber==4){
+      round(3,7000);
+    }
+    else if (roundNumber==5){
+      round(3,6000);
+    }
+    else if (roundNumber==6){
+      win=1;
+      gameOver();
+    }
+  }
+  // game==-1 means event handler senses Game Stopped
   else if (game==-1){
     game=0;
-    clearInterval(intervalDemand);
+    document.getElementById("time").innerHTML = "0.00";
+    roundNumber=0;
     clearInterval(intervalGrid);
     clearInterval(intervalCount);
+    clearInterval(intervalCheckRound);
+    grid_phase=1;
+    $("#gp-1").removeClass("bg-success").addClass("bg-warning");
+    $("#gp-2").removeClass("bg-warning").addClass("bg-success");
+    $("#gp-3").removeClass("bg-warning").addClass("bg-success");
+    $("#gp-4").removeClass("bg-warning").addClass("bg-success");
+    // ADD SHOW WIN OR LOSE IN WHAT ROUND in bootstrap popup modal? Sound effects
+    win=0;
   }
 }
